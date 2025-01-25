b9e88c7307a70cd86abf6cc2d2d5bf440de9caad
menosgrande00 <135849878+menosgrande00@users.noreply.github.com>
2025-01-25 14:03:33 +0300

    Add files via upload

---
+++ minitalk/minitalk.h
@@ -0,0 +1,13 @@
+#ifndef MINITALK_H
+# define MINITALK_H
+
+#include "signal.h"
+#include "unistd.h"
+#include "stdlib.h"
+#include "ft_printf/ft_printf.h"
+
+int	ft_atoi(const char *str);
+void ascii_to_byte(int pid, char c);
+void convert(int sig);
+
+#endif
