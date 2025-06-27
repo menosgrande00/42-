66c5e4a7c97976e2bdaa5f5d36da6fc00cf2d67b
menosgrande00 <135849878+menosgrande00@users.noreply.github.com>
2025-06-27 15:51:28 +0300

    Add files via upload

---
+++ minikabuk/src/env/environment.h
@@ -0,0 +1,18 @@
+#ifndef ENVIRONMENT_H
+# define ENVIRONMENT_H
+
+# include "minishell.h"
+
+typedef struct s_env
+{
+	char            *key;
+	char            *value;
+	struct s_env    *next;
+}		t_env;
+
+t_env	*init_env(char **envp);
+void	print_envs_alphabetic(t_env *env);
+t_env	*env_node(char *str);
+t_env	**env_to_array(t_env *env);
+
+#endif
