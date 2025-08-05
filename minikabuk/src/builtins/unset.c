2b6d12e105668be8d40cc7cf66bc87a0a5f10fc8
menosgrande00 <135849878+menosgrande00@users.noreply.github.com>
2025-08-05 17:27:59 +0300

    Add files via upload

---
+++ minikabuk/src/builtins/unset.c
@@ -0,0 +1,31 @@
+#include "minishell.h"
+
+int	ft_unset(t_minishell *ms, char *current_tok)
+{
+	t_env	*prev;
+	t_env	*it;
+	
+	if (!current_tok)
+		return (0);
+	if (!is_valid_identifier(current_tok))
+		return (report_not_valid(ms, current_tok));
+	prev = NULL;
+	it = ms->envp;
+	while (it)
+	{
+		if (!ft_strncmp(it->key, current_tok, ft_strlen(it->key) - 1))
+		{
+			if (prev)
+				prev->next = it->next;
+			else
+				ms->envp = it->next;
+			free(it->key);
+			free(it->value);
+			free(it);
+			break;
+		}
+		prev = it;
+		it = it->next;
+	}
+	return (0);
+}
