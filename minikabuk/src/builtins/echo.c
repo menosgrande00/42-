3ebc31375a0e363eb78fc9bdbabd366e1dbab8c9
menosgrande00 <135849878+menosgrande00@users.noreply.github.com>
2025-08-05 20:13:00 +0300

    Add files via upload

---
+++ minikabuk/src/builtins/echo.c
@@ -0,0 +1,102 @@
+#include "minishell.h"
+
+int	ft_echo(char *tok)
+{
+	int	i;
+	int	no_newline;
+	int	j;
+
+	i = 0;
+	no_newline = 0;
+	while (tok[i] == '-' && tok[i + 1] == 'n')
+	{
+		j = i + 2;
+		while (tok[j] == 'n')
+			j++;
+		if (tok[j] == ' ' || tok[j] == '\t' || tok[j] == '\0')
+		{
+			no_newline = 1;
+			i = j;
+			while (tok[i] == ' ' || tok[i] == '\t')
+				i++;
+		}
+		else
+			break;
+	}
+	while (tok[i])
+		write(1, &tok[i++], 1);
+	if (!no_newline)
+		write(1, "\n", 1);
+	return (0);
+}
+
+int		process_for_echo(t_token_list **tmp)
+{
+	char	*current;
+	char	*temp;
+	int		return_value;
+	int		first;
+
+	current = ft_strdup("");
+	if (!current)
+			return (1);
+	first = 1;
+	*tmp = (*tmp)->next;
+	while (*tmp && (*tmp)->token->type == TOKEN_WORD)
+	{
+		if (!first)
+		{
+			temp = ft_strjoin(current, " ");
+			free(current);
+	    	current = temp;
+		}
+	    if ((*tmp)->token->value)
+    		temp = (*tmp)->token->value;
+		else
+    		temp = "";
+		temp = ft_strjoin(current, temp);
+	    free(current);
+	    current = temp;
+	    *tmp = (*tmp)->next;
+	    first = 0;
+	}
+	return_value = ft_echo(current);
+	free(current);
+	return (return_value);
+}
+
+int	ft_echo_from_cmd_array(char **cmd)
+{
+	int i = 1;
+	int no_newline = 0;
+	int first = 1;
+
+	while (cmd[i] && cmd[i][0] == '-' && cmd[i][1] == 'n')
+	{
+		int j = 2;
+		while (cmd[i][j] == 'n')
+			j++;
+		if (cmd[i][j] == '\0')
+		{
+			no_newline = 1;
+			i++;
+		}
+		else
+		    break;
+	}
+	while (cmd[i] &&
+	    ft_strcmp(cmd[i], ">>") != 0 &&
+	    ft_strcmp(cmd[i], ">") != 0 &&
+	    ft_strcmp(cmd[i], "<") != 0 &&
+	    ft_strcmp(cmd[i], "<<") != 0)
+	{
+	    if (!first)
+	        write(1, " ", 1);
+	    write(1, cmd[i], ft_strlen(cmd[i]));
+	    first = 0;
+	    i++;
+	}
+	if (!no_newline)
+	    write(1, "\n", 1);
+	return (0);
+}
---
+++ minikabuk/src/parser/quotes.c
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   quotes.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omerfarukonal <omerfarukonal@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/29 19:55:48 by omerfarukon       #+#    #+#             */
+/*   Updated: 2025/08/04 12:06:48 by omerfarukon      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static int	create_single_quote_token(t_minishell *minishell, int start,
+										int i, t_token **current_token)
+{
+	char	*tmp;
+
+	tmp = ft_substr(minishell->input, start, i - start);
+	if (!tmp)
+		return (1);
+	*current_token = ft_calloc(sizeof(t_token), 1);
+	if (!*current_token)
+	{
+		free(tmp);
+		return (1);
+	}
+	(*current_token)->value = tmp;
+	(*current_token)->type = TOKEN_WORD;
+	(*current_token)->quotes = 1;
+	return (0);
+}
+
+static int	sqm(t_minishell *ms, int *i, t_token **current_token, int j)
+{
+	if (ms->input[j - 1] == ' ' && (ms->input[*i + 1] == ' '
+			|| ms->input[*i + 1] == '\0'))
+	{
+		add_token_to_list(&ms->token_list, *current_token);
+		(*i)++;
+		return (0);
+	}
+	if (ms->input[j - 1] != ' ')
+	{
+		merge_with_previous_token(ms, current_token);
+		if ((ms->input[*i + 1] != ' '
+				&& ms->input[*i + 1] != '\0'))
+		{
+			handle_after_quote_text(ms, i, j);
+			while (ms->input[*i] != ' ' && ms->input[*i] != '\0')
+				(*i)++;
+		}
+	}
+	else if ((ms->input[*i + 1] != ' ' && ms->input[*i + 1] != '\0'))
+	{
+		remove_quotes_from_input(ms, j, *i, '\'');
+		*i = j - 2;
+	}
+	(*i)++;
+	return (0);
+}
+
+static int	process_single_quote_content(t_minishell *minishell, int *i)
+{
+	while (minishell->input[*i] && minishell->input[*i] != '\'')
+		(*i)++;
+	if (minishell->input[*i] != '\'')
+	{
+		write(2, "syntax error: unclosed quote\n", 29);
+		minishell->exit_status = 2;
+		return (1);
+	}
+	return (0);
+}
+
+int	single_quotes(t_minishell *minishell, int *i, t_token **current_token)
+{
+	int	start;
+	int	j;
+	int	ret;
+
+	j = (*i);
+	start = ++(*i);
+	ret = process_single_quote_content(minishell, i);
+	if (ret)
+		return (ret);
+	ret = create_single_quote_token(minishell, start, *i, current_token);
+	if (ret)
+		return (1);
+	ret = sqm(minishell, i, current_token, j);
+	return (ret);
+}
